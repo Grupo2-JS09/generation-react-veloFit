a739817e3c0cb495f206117de1d174ec5dde2143
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-11-10 13:22:04 -0400

    build: estilização do footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria";
+import FormCategoria from "./components/categoria/formcategoria/FormCategoria";
+import ListaCategoria from "./components/categoria/listacategoria/ListaCategoria";
 import DeletarServico from "./components/servico/deletarservico/DeletarServico";
 import FormServico from "./components/servico/formservico/FormServico";
 import ListaServico from "./components/servico/listaservico/ListaServico";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
-function App() {
+export function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="min-h-[80vh]">
+        <Navbar />
+        <div className='min-h-[80vh]'>
           <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<ListaServico />} />
+            <Route path="/servicos" element={<ListaServico />} />
             <Route path="/cadastrarservico" element={<FormServico />} />
             <Route path="/editarservico" element={<FormServico />} />
             <Route path="/deletarservico/:id" element={<DeletarServico />} />
+            <Route path="/categorias" element={<ListaCategoria />} />
+            <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+            <Route path="/editarcategoria" element={<FormCategoria />} />
+            <Route
+              path="/deletarcategoria/:id"
+              element={<DeletarCategoria />}
+            />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
 }
-
-export default App
+export default App;
