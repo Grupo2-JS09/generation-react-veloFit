85cc9d3f3a02f479a0c7c21eadf638f01edede65
benner.dias <bennerheberth@gmail.com>
2025-11-10 14:06:28 -0300

    style of Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,30 +3,28 @@ import React from "react";
 function Login() {
   return (
     <>
-      <div className="flex items-center justify-center bg-[url('https://i.pinimg.com/1200x/77/cd/98/77cd9852179cf373847b46869b3ffbd5.jpg')] bg-cover bg-center bg-no-repeat relative isolate min-h-screen ">
-        <div className='absolute inset-0 bg-gradient-to-b from-[rgba(42,21,14,0.45)] to-[rgba(42,21,14,0.55)] pointer-events-none'></div>
-        <div className='flex flex-col bg-black rounded-lg text-white w-96 h-120 justify-center items-center '>
+      <div className="flex items-center justify-center bg-[url('https://64.media.tumblr.com/tumblr_m1me4nvOwe1rsbhe4o1_500.gif')] bg-cover bg-center bg-no-repeat relative isolate min-h-screen ">
+        <div className='flex flex-col bg-transparent shadow-[0_0_40px_rgba(0,0,0,0.8)] rounded-3xl text-white w-96 h-120 justify-center items-center'>
           <div className="w-1/3 h-1/3">
             <img src="https://images.icon-icons.com/1378/PNG/512/avatardefault_92824.png" alt="" />
           </div>
-          <form>
-            <div className="flex justify-items-center p-5">
-              <label htmlFor='email'>Email </label>
+          <form className="flex flex-col">
+            <div className="flex justify-items-center p-5 gap-5">
               <input
                 type='text'
                 name='usuario'
                 required={true}
                 placeholder='Insira seu email'
-                className="border-1"
+                className="border-b w-60 rounded-md pl-2"
               />
             </div>
-            <div className="flex justify-items-center p-5">
-              <label htmlFor='password'>Senha </label>
+            <div className="flex justify-items-center p-5 gap-5">
               <input
                 type='password'
                 name='password'
                 required={true}
-                className="border-1"
+                className="border-b w-60 rounded-md pl-2"
+                placeholder='Insira sua senha'
               />
             </div>
           </form>
