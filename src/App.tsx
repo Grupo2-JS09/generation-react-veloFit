90e855c904e8c1974e4c50c17a642f7927d44f62
Phcode007 <paulobelarmino047@gmail.com>
2025-11-10 11:29:29 -0300

    feat: Adicionar componente Navbar com links e botões
    Implementa a Navbar com links para Home, Serviços, Sobre, Contato e botões de Login e Cadastro, utilizando formato de function.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className='min-h-[80vh]'>
+        <Navbar />
+        <div className="min-h-[80vh]">
           <Routes>
-            <Route path='/home' element={<Home />} />
-            <Route path='/login' element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
