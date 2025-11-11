0b50c7fab776690eaa3ce8b132d4fe55e5ab49fe
Anna Clara <andradeannac@gmail.com>
2025-11-11 14:54:20 -0300

    style: categorias

--- src/components/categoria/listacategoria/ListaCategoria.tsx
+++ src/components/categoria/listacategoria/ListaCategoria.tsx
@@ -52,13 +52,18 @@ function ListaCategoria() {
 
   return (
     <>
-      <div className='min-h-screen bg-gradient-to-b from-slate-800 via-slate-700 to-slate-900 text-white'>
-        <div className='flex flex-col items-center min-h-screen py-10 px-6'>
+      <div className='relative min-h-screen  text-white'>
+
+        <div className='absolute inset-0 bg-[url("https://s2-g1.glbimg.com/MTcUSm8am3UgA3nlYocWoYYTnWE=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/i/9/gzuvUhRjqAN0sT8sb5Zg/250312-montagem-exercicios.jpg")] bg-cover bg-center filter grayscale'></div>
+
+        <div className="absolute inset-0 bg-black/40"></div>
+
+        <div className='relative z-10 flex flex-col items-center min-h-screen py-10 px-6'>
           <h1 className='text-4xl font-bold mb-12 text-center tracking-tight'>
             Categorias
           </h1>
 
-          {isLoading && <SyncLoader color='#74b9ff' size={32} />}
+          {isLoading && <SyncLoader color='#9ae19d' size={32} />}
 
           {!isLoading && categorias.length === 0 && (
             <span className='text-2xl text-center my-8'>
@@ -68,7 +73,7 @@ function ListaCategoria() {
 
           <div className='flex justify-end w-full max-w-6xl mb-10'>
             <Link to='/cadastrarcategoria'>
-              <button className='px-6 py-2 rounded-lg bg-orange-500 hover:bg-orange-600 transition font-semibold text-sm text-white shadow-md'>
+              <button className='px-6 py-2 rounded-lg bg-(--tomato) hover:bg-(--jet) transition font-semibold text-sm text-white shadow-md'>
                 Cadastrar nova categoria
               </button>
             </Link>
