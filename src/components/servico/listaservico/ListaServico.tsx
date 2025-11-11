dab794d423fb01b63fe4f78b71c8aa63ceb37a32
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-11-11 17:05:34 -0300

    style: CardServico and ListaServico + build: Contatos page

--- src/components/servico/listaservico/ListaServico.tsx
+++ src/components/servico/listaservico/ListaServico.tsx
@@ -4,8 +4,8 @@ import { SyncLoader } from "react-spinners";
 import { AuthContext } from "../../../contexts/AuthContext";
 import type Servico from "../../../models/Servico";
 import { buscar } from "../../../services/Service";
-import CardServicos from "../cardservico/CardServico";
 import { ToastAlerta } from "../../../utils/ToastAlerta";
+import CardServicos from "../cardservico/CardServico";
 
 function ListaServicos() {
   const navigate = useNavigate();
@@ -15,7 +15,6 @@ function ListaServicos() {
   const [servicos, setServicos] = useState<Servico[]>([]);
 
   const { usuario, handleLogout } = useContext(AuthContext);
-  
   const token = usuario.token;
 
   useEffect(() => {
@@ -46,40 +45,39 @@ function ListaServicos() {
   }
 
   return (
-    <>
-      {isLoading && (
-        <div className="flex justify-center w-full my-8">
-          <SyncLoader color="#312e81" size={32} />
-        </div>
-      )}
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80')] bg-cover bg-center filter grayscale"></div>
 
-      <div className="flex justify-center w-full my-4">
-        <div className="container flex flex-col">
-          {!isLoading && servicos.length === 0 && (
-            <span className="text-3xl text-center my-8">
-              Nenhum Servico foi encontrado!
-            </span>
-          )}
-
-          <div className='flex justify-end w-full max-w-6xl mb-10'>
-            <Link to='/cadastrarservico'>
-              <button className='px-6 py-2 rounded-lg bg-(--tomato) hover:bg-orange-600 transition font-semibold text-sm text-white shadow-md'>
-                Cadastrar Novo Serviço
-              </button>
-            </Link>
+      <div className="relative z-10 flex flex-col items-center pt-10">
+        {isLoading && (
+          <div className="flex justify-center w-full my-8">
+            <SyncLoader color="#283a2b" size={32} />
           </div>
+        )}
 
-          <div
-            className="grid grid-cols-1 md:grid-cols-2
-                                    lg:grid-cols-3 gap-8"
-          >
-            {servicos.map((servicos) => (
-              <CardServicos key={servicos.id} servico={servicos} />
+        {!isLoading && servicos.length === 0 && (
+          <span className="text-3xl text-center my-8">
+            Nenhum serviço foi encontrado!
+          </span>
+        )}
+
+        <div className="flex justify-end w-full max-w-6xl mb-10">
+          <Link to="/cadastrarservico">
+            <button className="px-6 py-2 rounded-lg bg-orange-500 hover:bg-orange-900 transition font-semibold text-sm text-white shadow-md">
+              Cadastrar Novo Serviço
+            </button>
+          </Link>
+        </div>
+
+        <div className="flex justify-center w-full">
+          <div className="grid place-items-center gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            {servicos.map((servico) => (
+              <CardServicos key={servico.id} servico={servico} />
             ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 export default ListaServicos;
