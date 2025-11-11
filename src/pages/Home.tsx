ab2eb40b1fdc275e5c8f33e508429c50fa4e2182
unknown <valerde88@outlook.com>
2025-11-11 11:30:10 -0300

    versão atualizada home com gif na home toda

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 
   return (
     <>
-      <div className=" w-full flex flex-col items-center justify-center gap-10 sm:flex-col md:flex-row md:items-stretch overflow-hidden min-h-screen bg-linear-to-r from-[#2b302e] via-[#3d5041] to-[#527859] ">
-        <div className="flex flex-col w-full md:w-1/2 justify-center items-center bg-[url('https://64.media.tumblr.com/tumblr_m1me4nvOwe1rsbhe4o1_500.gif')] bg-cover bg-center bg-no-repeat rounded-2xl mt-1 p-4  ">
+      <div className=" w-full flex flex-col items-center justify-center gap-10 sm:flex-col md:flex-row md:items-stretch overflow-hidden min-h-screen bg-[url('https://64.media.tumblr.com/tumblr_m1me4nvOwe1rsbhe4o1_500.gif')] bg-cover bg-center bg-no-repeat  ">
+        <div className="flex flex-col w-full md:w-1/2 justify-center items-center  ">
           <div className="w-1/2 flex flex-col gap-5 py-5">
             <div className="flex flex-col gap-6">
               <h1 className="text-3xl text-white font-bold sm:text-6xl ">
